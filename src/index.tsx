0b1aad17a911f76224cb46b831c73861aad51a40
Sergio Xalambrí <hello@sergiodxa.com>
2020-10-27 20:10:55 -0500

    Support non-string messages

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import { useEffect } from 'react';
+import useConsistentValue from 'use-consistent-value';
 
 export type Level =
   | 'log'
@@ -14,30 +15,36 @@ export interface UseLogConfig {
   shouldLogInProduction?: boolean;
 }
 
-export default function useLog(message: string, config?: UseLogConfig): void;
-export default function useLog(
-  message: string,
+export function useLog<Message = any>(
+  message: Message,
+  config?: UseLogConfig
+): void;
+export function useLog<Message = any>(
+  message: Message,
   { level = 'log', shouldLogInProduction = false }: UseLogConfig = {}
 ): void {
-  React.useEffect(() => {
+  const value = useConsistentValue(message);
+  useEffect(() => {
     if (!shouldLogInProduction && process.env.NODE_ENV === 'production') return;
     switch (level) {
       case 'error':
-        return console.error(message);
+        return console.error(value);
       case 'warn':
-        return console.warn(message);
+        return console.warn(value);
       case 'info':
-        return console.info(message);
+        return console.info(value);
       case 'debug':
-        return console.debug(message);
+        return console.debug(value);
       case 'dir':
-        return console.dir(message);
+        return console.dir(value);
       case 'table':
-        return console.table(message);
+        return console.table(value);
       case 'log':
-        return console.log(message);
+        return console.log(value);
       default:
         throw new Error('Invalid log level');
     }
-  }, [message, level, shouldLogInProduction]);
+  }, [value, level, shouldLogInProduction]);
 }
+
+export default useLog;
